9c3c8415cabeb24f73960b26eb7ed1f3c5dbb3ed
Paul Mascall <pjwmascall@hotmail.com>
2022-02-17 15:22:13 +0000

    created handleFormSubmit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const addToList = function(title, author, category) {
+    const unorderedList = document.querySelector('#reading-list');
+    console.log('it worked')
+  }
+
+  const handleFormSubmit = function (event) {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const author = event.target.author.value;
+    const category = event.target.category.value;
+    addToList()
+    console.log(event.target.title.value, event.target.author, event.target.category)
+  }
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
 })
