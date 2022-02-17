adbf1c62072b5addaaebbe8683f6d9725a771767
Steph Paterson <steph.c.paterson@gmail.com>
2022-02-17 15:41:11 +0000

    shows form input in a list item

--- js/app.js
+++ js/app.js
@@ -3,7 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const addToList = function(title, author, category) {
     const unorderedList = document.querySelector('#reading-list');
-    console.log('it worked')
+    const listItem = document.createElement('li')
+    const titleHeading = document.createElement('h2')
+    titleHeading.textContent = title
+    const authorHeading = document.createElement('h3')
+    authorHeading.textContent = author
+    const categoryHeading = document.createElement('h4')
+    categoryHeading.textContent = category
+    listItem.appendChild(titleHeading)
+    listItem.appendChild(authorHeading)
+    listItem.appendChild(categoryHeading)
+    unorderedList.appendChild(listItem)
   }
 
   const handleFormSubmit = function (event) {
@@ -11,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const title = event.target.title.value;
     const author = event.target.author.value;
     const category = event.target.category.value;
-    addToList()
+    addToList(title, author, category)
     console.log(event.target.title.value, event.target.author, event.target.category)
   }
 
